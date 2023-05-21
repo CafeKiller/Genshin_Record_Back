cfff740159a9291086b19d92807188e96ea82189
CoffeeKiller <18320210361@163.com>
2023-05-21 15:27:12 +0800

    添加武器池连接模块

---
+++ src/models/WeaponPoolModel.ts
@@ -0,0 +1,19 @@
+import * as mongoose from 'mongoose';
+import { WeapPoolScheme } from "../schemas/weapPoolScheme";
+import {Model} from "mongoose";
+
+const weaponPool:Model<any> = mongoose.model('weap_pools',WeapPoolScheme)
+
+export default class RoleModel{
+    // 获取全部元素
+    public getAll = async ():Promise<any> => {
+        return await weaponPool.find();
+    }
+    //
+    public find = async (key:any):Promise<any> =>{
+        console.log(key)
+        return await weaponPool.find(key)
+    }
+
+
+}
